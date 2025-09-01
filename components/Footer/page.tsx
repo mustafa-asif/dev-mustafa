5c48646d5fc2a65375b39a55b32ae7526d15f18d
mustafa <mustafawarind03@gmail.com>
2025-09-01 17:16:18 +0500

    add social media links

--- components/Footer/page.tsx
+++ components/Footer/page.tsx
@@ -1,7 +1,18 @@
- export function Footer() {
+import Link from 'next/link';
+import {FaGithub, FaLinkedin} from 'react-icons/fa';
+
+export function Footer() {
+
   return (
-    <footer className="w-full py-6 mt-20 bg-gray-100 text-center text-gray-600 text-sm">
-      <p>© {new Date().getFullYear()} Mustafa. All rights reserved.</p>
+    <footer className=" flex flex-row justify-center w-full py-6 mt-20 bg-gray-100 text-center text-gray-600 text-sm">
+     <Link href="https://github.com/mustafa-asif">
+      <FaGithub className="inline mx-2 size-6 hover:text-gray-800" />
+     </Link>
+     <Link href="https://www.linkedin.com/in/mustafa-asif-warind/">
+      <FaLinkedin className="inline mx-2 size-6 hover:text-gray-800" />
+     </Link>
+
+      <p className='text-center'>© {new Date().getFullYear()} Mustafa. All rights reserved.</p>
     </footer>
   );
 }
